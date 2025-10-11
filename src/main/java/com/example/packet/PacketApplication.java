e53fa3e4730a2c2e467fab6dcb84a5099bcd011b
AlexandrStepchenkov <armageddon50901@gmail.com>
2025-10-11 17:38:07 +0300

    3

---
+++ src/main/java/com/example/packet/PacketApplication.java
@@ -0,0 +1,13 @@
+package com.example.packet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacketApplication.class, args);
+	}
+
+}
